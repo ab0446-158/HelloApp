e0e9a01ee4c6ed1e47388a3f79e7d1dad0aa8222
ab0446-158 <ab0446@srmist.edu.in>
2026-03-17 10:49:00 +0530

    Added UC4

---
+++ UC4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UC4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Numbers from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
+        }
+
+        sc.close();
+    }
+}
